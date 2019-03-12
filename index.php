611c05322960dc73dc1755be4547102fe3eda9a4
vkosmos <korolev.dm.va@gmail.com>
2019-03-13 00:30:09 +0300

    Выполено 6 задание. Вывод отдельной новости с полным текстом.

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 
 use App\Models\Article;
-use App\Models\User;
 
 require __DIR__ . '/autoload.php';
 
-//$data = Article::findAll();
 $data = Article::findN(3);
 
 include(__DIR__ . '/templates/index.php');
